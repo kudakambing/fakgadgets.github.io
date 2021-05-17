64eef59b321331681aa040076c2691855e474811
kudakambing <84294050+kudakambing@users.noreply.github.com>
2021-05-17 13:55:21 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
   setInterval( function() {
 
     var hours    = new Date().getHours();
-    //var seconds  = new Date().getSeconds();
+    var seconds  = new Date().getSeconds();
     var minutes  = new Date().getMinutes();
 
-    //var realTime = ( hours < 10 ? '0' : '' ) + hours + ' : ' + ( minutes < 10 ? '0' : '' ) + minutes + ' : ' + ( seconds < 10 ? '0' : '' ) + seconds
-    var realTime = ( hours < 10 ? '0' : '' ) + hours + ' : ' + ( minutes < 10 ? '0' : '' ) + minutes
+    var realTime = ( hours < 10 ? '0' : '' ) + hours + ' : ' + ( minutes < 10 ? '0' : '' ) + minutes + ' : ' + ( seconds < 10 ? '0' : '' ) + seconds
+    
     $('.time').html(realTime);
     $('.time').attr('data-time', realTime);
 
